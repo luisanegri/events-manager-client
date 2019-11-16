3d5e63463a5a2c8e112596b40ea26944e026f6e0
luisanegri <luisaanegri@gmail.com>
2019-11-16 15:04:25 +0100

    add routes for list of events, events details, and create event pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import EventsListContainer from './components/EventsListContainer';
+import CreateEventFormContainer from './components/CreateEventFormContainer';
+import EventDetailsContainer from './components/EventDetailsComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route path="/" exact component={EventsListContainer} />
+        <Route path="/event" exact component={CreateEventFormContainer} />
+        <Route path="/event/:id" component={EventDetailsContainer} />
+      </div>
+    );
+  }
 }
-
-export default App;
